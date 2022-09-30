5bc6fd0e5868c3fd7745aaf875c15376723519f8
rileyh01 <rileyjholt@gmail.com>
2022-09-29 21:20:43 -0400

    "Added Board"

--- game.py
+++ game.py
@@ -1,1 +1,47 @@
 import random
+
+#Creates Game piece class
+class GamePiece:
+    pass
+
+#Creates Player Class
+class Player:
+    pass
+
+#game piece visual icons
+red = "(R)"
+blue = "(B)"
+empty = "   "
+
+board = []
+def generate_board(x = 7, y = 6):
+    board.clear()
+    for index in range(0, x, 1):
+        board.append([])
+        for indexy in range(0, y, 1):
+            board[index].append(empty)
+
+generate_board(7, 6)
+#print(board)
+
+def print_board():
+  print("""
+     *****************************
+     *{}*{}*{}*{}*{}*{}*{}*
+     *****************************
+     *{}*{}*{}*{}*{}*{}*{}*
+     *****************************
+     *{}*{}*{}*{}*{}*{}*{}*
+     *****************************
+     *{}*{}*{}*{}*{}*{}*{}*
+     *****************************
+     *{}*{}*{}*{}*{}*{}*{}*
+     *****************************
+     *{}*{}*{}*{}*{}*{}*{}*
+     *****************************
+     *                           *
+    ***                         ***
+  """.format(empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty,))
+
+print_board()
+#print(empty_spot)
